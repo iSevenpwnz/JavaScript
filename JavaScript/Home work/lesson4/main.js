e0b1ceb4d4a79e483db4d2c374d9845bed1eb8ed
iseve <isevenpwnz@gmail.com>
2021-11-18 14:55:17 +0200

    hw4

---
+++ JavaScript/Home work/lesson4/main.js
@@ -0,0 +1,28 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// function Rect(a, b) {
+// return a*b
+// }
+// let result = Rect(4,6)
+// document.write(result)
+// або
+// document.write(Rect(4,6))
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+// function round(Pi, r) {
+//     return Pi * Math.pow(r,2)
+// }
+// let result = round(3.14,4)
+// document.write(result)
+// або
+// document.write(round(3.14,4))
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+// function cylinder(Pi, r, h) {
+//     return 2 * Pi * r * (h + r);
+// }
+// let result = cylinder(3.14,3,5)
+// document.write(result)
+// або
+// document.write(cylinder(3.14,3,5))
