a1e1f135a053ff69bef3db867b2e50435bdf57e9
iseve <isevenpwnz@gmail.com>
2021-11-22 18:15:33 +0200

    hw4

---
+++ JavaScript/Class work/lesson4/main.js
@@ -0,0 +1,37 @@
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+// function numsMin(num1, num2, num3) {
+//     if (num1 < num2 && num1 < num3) {
+//         console.log(num1)
+//     } else if (num2 < num3 && num2 < num1) {
+//         console.log(num2);
+//     } else {
+//         console.log(num3)
+//     }
+// }
+//  numsMin(80,50,60 )
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+// function numsMax(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         console.log(num1);
+//     }
+//         else if (num2 > num1 && num2 > num3) {
+//         console.log(num2);
+//     } else {
+//         console.log(num3)
+//     }
+// }
+// numsMax(40,20,41)
+
+// - створити функцію яка повертає найбільше число з масиву
+let nums = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+function numberMax(arr) {
+    for (let arrElement of arr) {
+
+    }
+        }
+        numberMax([4])
+
+    
+
